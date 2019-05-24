29f560692394be62695f335bc1ac1893486d863c
chenghongqiang <962691576@qq.com>
2019-05-24 14:54:17 +0800

    Initial commit

---
+++ src/main/java/com/chq/project/management/ManagementPlatformApplication.java
@@ -0,0 +1,19 @@
+package com.chq.project.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台管理系统
+ * 启动类
+ *
+ * @author CHQ
+ */
+@SpringBootApplication
+public class ManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementPlatformApplication.class, args);
+    }
+
+}
